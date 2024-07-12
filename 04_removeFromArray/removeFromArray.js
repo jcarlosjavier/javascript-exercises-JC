d550ef946156232aa99753317f057e293d42a207
Juan Carlos Javier <127345680+jcarlosjavier@users.noreply.github.com>
2024-07-11 23:50:39 -0400

    initial commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    const arrFinal = arr.filter(Element => Element !== values);
+    const arrType = arr.filter(Element => !values.includes(Element));
+    return arrFinal, arrType;  
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
